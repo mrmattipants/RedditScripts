6d29bce7d8dd2df436c1aec7b2baaca5f90f0dbf
Matti Pedersen <mrmattipants@gmail.com>
2023-03-18 20:00:37 -0500

    Add files via upload

---
+++ PowerCfg/Update-LaptopPowerConfiguration.ps1
@@ -0,0 +1,23 @@
+﻿$ActiveScheme = (powercfg.exe /getactivescheme)
+$ActiveSchemeGUID = $ActiveScheme.split(" ")
+$ActiveSchemeGUID = $ActiveSchemeGUID[3]
+
+$PowerSaver = (powercfg.exe /List | FIND "Power saver")
+$PowerSaverGUID = $PowerSaver.split(" ")
+$PowerSaverGUID = $PowerSaverGUID[3]
+
+$HighPerformance = (powercfg.exe /List | FIND "High performance")
+$HighPerformanceGUID = $HighPerformance.split(" ")
+$HighPerformanceGUID = $HighPerformanceGUID[3]
+
+$BatteryStatus = (Get-CimInstance Win32_Battery).BatteryStatus
+
+If ($BatteryStatus -eq 1) {
+    If ($ActiveSchemeGUID -ne $PowerSaverGUID) {
+        powercfg.exe /setactive "$($PowerSaverGUID)"
+    }
+} Else {
+    If ($ActiveSchemeGUID -ne $HighPerformanceGUID) {
+        powercfg.exe /setactive "$($HighPerformanceGUID)"
+    }
+}
