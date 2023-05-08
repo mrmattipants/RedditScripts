750a400091f92c04b4526715e11717672647312a
Matti Pedersen <mrmattipants@gmail.com>
2023-05-08 11:53:26 -0500

    Delete Print3D/Uninstall-Print3D directory

--- Print3D/Uninstall-Print3D/Uninstall-Print3DMicrosoftStoreApp(DISM).ps1
+++
@@ -1,9 +0,0 @@
-﻿$Print3d = DISM /Online /Get-ProvisionedAppxPackages | findstr "Microsoft.Print3D"
-
-If ($Print3d) {
-    
-    $Print3dPackage = $Print3d[1].split(":")[1].replace(" ", "")
-
-    Dism /Online /Remove-ProvisionedAppxPackage /PackageName:$Print3dPackage
-
-}
