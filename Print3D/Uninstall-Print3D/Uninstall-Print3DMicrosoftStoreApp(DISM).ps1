6ad9272765ccda3bd26af04f5c20dd29c81fa0f1
Matti Pedersen <mrmattipants@gmail.com>
2023-05-08 15:27:34 -0500

    Add files via upload

---
+++ Print3D/Uninstall-Print3D/Uninstall-Print3DMicrosoftStoreApp(DISM).ps1
@@ -0,0 +1,15 @@
+﻿$Print3d = DISM /Online /Get-ProvisionedAppxPackages | findstr "Microsoft.Print3D"
+
+If ($Print3d) {
+    
+    $Print3dPackage = $Print3d[1].split(":")[1].replace(" ", "")
+
+    Dism /Online /Remove-ProvisionedAppxPackage /PackageName:$Print3dPackage
+
+    Write-Host "The Print 3D Microsoft Store App has been Removed from this System" -ForegroundColor Green
+
+} Else {
+
+    Write-Host "The Print 3D Microsoft Store App is not Installed on this System" -ForegroundColor Yellow
+
+}
