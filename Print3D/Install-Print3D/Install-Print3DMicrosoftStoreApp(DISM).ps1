39ae8d3d4cde526509e8830a7ee131bb48038730
Matti Pedersen <mrmattipants@gmail.com>
2023-05-08 15:39:59 -0500

    Add files via upload

---
+++ Print3D/Install-Print3D/Install-Print3DMicrosoftStoreApp(DISM).ps1
@@ -0,0 +1,13 @@
+﻿$Print3d = DISM /Online /Get-ProvisionedAppxPackages | findstr "Microsoft.Print3D"
+
+If (!$Print3D) {
+
+    DISM.EXE /Online /Add-ProvisionedAppxPackage /PackagePath:"$($PSScriptRoot)\Microsoft.Print3D_3.3.791.0_neutral_~_8wekyb3d8bbwe.AppxBundle" /DependencyPackagePath:"$($PSScriptRoot)\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx" /SkipLicense
+
+    Write-Host "The Print 3D Microsoft Store App has been Successfully Installed" -ForegroundColor Green
+
+} Else {
+
+    Write-Host "The Print 3D Microsoft Store App is already Installed" -ForegroundColor Yellow
+
+}
---
+++ Print3D/Install-Print3D/Install-Print3DMicrosoftStoreAppxProvisionedPackage.ps1
@@ -0,0 +1,13 @@
+﻿$Print3D = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.Print3D"}
+
+If (!$Print3D) {
+
+        Add-AppxProvisionedPackage -Online -PackagePath "$($PSScriptRoot)\Microsoft.Print3D_3.3.791.0_neutral_~_8wekyb3d8bbwe.AppxBundle" -DependencyPackagePath "$($PSScriptRoot)\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx" -SkipLicense
+        
+        Write-Host "The Print 3D Microsoft Store App has been Installed to this System" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Print 3D Microsoft Store App is already Installed on this System" -ForegroundColor Yellow
+
+}
