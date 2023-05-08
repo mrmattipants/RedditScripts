35c2dd02a7f538e7b02a1b8dc13e73c18c88f455
Matti Pedersen <mrmattipants@gmail.com>
2023-05-08 10:25:09 -0500

    Add files via upload

---
+++ Print3D/Uninstall-Print3D/Uninstall-Print3DMicrosoftStoreApp(WinGet).ps1
@@ -0,0 +1,12 @@
+﻿$Print3d = (winget list --query "Print3D" | findstr "Print3D")
+
+If ($Print3d) {
+
+    $P3dInfo = $Print3d.split(" ")
+    $P3dName = $P3dInfo[0]
+    $P3dId = $P3dInfo[1]
+    $P3dVersion = $P3dInfo[2]
+
+    winget uninstall --name $P3dName --id $P3dId --version $P3dVersion
+
+}
---
+++ Print3D/Uninstall-Print3D/Uninstall-Print3DMicrosoftStoreApp.ps1
@@ -0,0 +1,13 @@
+﻿$Print3D = Get-AppxPackage -Name Microsoft.Print3D -AllUsers
+
+If ($Print3D) {
+
+        Remove-AppxPackage -Package $Print3D.PackageFullName -AllUsers -Confirm:$False
+        
+        Write-Host "The Print 3D Microsoft Store App has been Removed from this System" -ForegroundColor Green
+
+} Else {
+    
+    Write-Host "The Print 3D Microsoft Store App is not Installed on this System" -ForegroundColor Yellow
+
+}
