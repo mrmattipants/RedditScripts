fc65282feb18746e29f982c62590a88dff71896e
Matti Pedersen <mrmattipants@gmail.com>
2023-05-29 19:19:44 -0500

    Add files via upload

---
+++ PS_PinToTaskbar/PinMSIEToTaskbar.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -ExecutionPolicy Bypass -File %~dp0PinToTaskBar1903.ps1 "C:\Program Files (x86)\Internet Explorer\iexplore.exe" PIN
+
+EXIT
---
+++ PS_PinToTaskbar/PinNotepadToTaskbar.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -ExecutionPolicy Bypass -Command %~dp0PinToTaskBar1903.ps1 "C:\Windows\system32\notepad.exe" PIN
+
+EXIT
