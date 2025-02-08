e6f34f37f6d006f19c53bd77cf4fd73358854cb5
Matti Pedersen <mrmattipants@gmail.com>
2025-02-08 13:18:50 -0600

    Add files via upload

---
+++ Get-MsiData/Get-MsiData.ps1
@@ -0,0 +1,103 @@
+﻿function Get-MsiFileMetaData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline)][Object] $File
+    )
+    Process {
+        foreach ($F in $File) {
+            $MetaDataObject = [ordered] @{}
+            if ($F -is [string]) {
+                $FileInformation = Get-ItemProperty -Path $F
+            } elseif ($F -is [System.IO.DirectoryInfo]) {
+                #Write-Warning "Get-FileMetaData - Directories are not supported. Skipping $F."
+                continue
+            } elseif ($F -is [System.IO.FileInfo]) {
+                $FileInformation = $F
+            } else {
+                Write-Warning "Get-FileMetaData - Only files are supported. Skipping $F."
+                continue
+            }
+            $ShellApplication = New-Object -ComObject Shell.Application
+            $ShellFolder = $ShellApplication.Namespace($FileInformation.Directory.FullName)
+            $ShellFile = $ShellFolder.ParseName($FileInformation.Name)
+            $MetaDataProperties = [ordered] @{}
+            0..400 | ForEach-Object -Process {
+                $DataValue = $ShellFolder.GetDetailsOf($null, $_)
+                $PropertyValue = (Get-Culture).TextInfo.ToTitleCase($DataValue.Trim()).Replace(' ', '')
+                if ($PropertyValue -ne '') {
+                    $MetaDataProperties["$_"] = $PropertyValue
+                }
+            }
+            foreach ($Key in $MetaDataProperties.Keys) {
+                $Property = $MetaDataProperties[$Key]
+                $Value = $ShellFolder.GetDetailsOf($ShellFile, [int] $Key)
+                if ($Property -in 'Attributes', 'Folder', 'Type', 'SpaceFree', 'TotalSize', 'SpaceUsed') {
+                    continue
+                }
+                If (($null -ne $Value) -and ($Value -ne '')) {
+                    $MetaDataObject["$Property"] = $Value
+                }
+            }
+            if ($FileInformation.VersionInfo) {
+                $SplitInfo = ([string] $FileInformation.VersionInfo).Split([char]13)
+                foreach ($Item in $SplitInfo) {
+                    $Property = $Item.Split(":").Trim()
+                    if ($Property[0] -and $Property[1] -ne '') {
+                        $MetaDataObject["$($Property[0])"] = $Property[1]
+                    }
+                }
+            }
+            $MetaDataObject["Attributes"] = $FileInformation.Attributes
+            $MetaDataObject['IsReadOnly'] = $FileInformation.IsReadOnly
+            $MetaDataObject['IsHidden'] = $FileInformation.Attributes -like '*Hidden*'
+            $MetaDataObject['IsSystem'] = $FileInformation.Attributes -like '*System*'
+            [PSCustomObject] $MetaDataObject
+        }
+    }
+}
+
+Function Get-MsiProductName {
+    param (
+        [parameter(Mandatory=$true, ValueFromPipeline)] 
+        [ValidateNotNullOrEmpty()] 
+            [System.IO.FileInfo] $MSIPATH
+    ) 
+    if (!(Test-Path $MSIPATH.FullName)) { 
+        throw "File '{0}' does not exist" -f $MSIPATH.FullName 
+    } 
+    try { 
+        $WindowsInstaller = New-Object -com WindowsInstaller.Installer 
+        $Database = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $Null, $WindowsInstaller, @($MSIPATH.FullName, 0)) 
+        $NameQuery = "SELECT Value FROM Property WHERE Property = 'ProductName'"
+        $View = $database.GetType().InvokeMember("OpenView", "InvokeMethod", $Null, $Database, ($NameQuery)) 
+        $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null) | Out-Null
+        $Record = $View.GetType().InvokeMember( "Fetch", "InvokeMethod", $Null, $View, $Null ) 
+        $Version = $Record.GetType().InvokeMember( "StringData", "GetProperty", $Null, $Record, 1 ) 
+        return $Version
+    } catch { 
+        throw "Failed to get MSI Product Name: {0}." -f $_
+    }       
+}
+
+Function Get-MsiProductVersion {
+    param (
+        [parameter(Mandatory=$true, ValueFromPipeline)] 
+        [ValidateNotNullOrEmpty()] 
+            [System.IO.FileInfo] $MSIPATH
+    ) 
+    if (!(Test-Path $MSIPATH.FullName)) { 
+        throw "File '{0}' does not exist" -f $MSIPATH.FullName 
+    } 
+    try { 
+        $WindowsInstaller = New-Object -com WindowsInstaller.Installer 
+        $Database = $WindowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $Null, $WindowsInstaller, @($MSIPATH.FullName, 0)) 
+        $VersionQuery = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
+        $View = $database.GetType().InvokeMember("OpenView", "InvokeMethod", $Null, $Database, ($VersionQuery)) 
+        $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null) | Out-Null
+        $Record = $View.GetType().InvokeMember( "Fetch", "InvokeMethod", $Null, $View, $Null ) 
+        $Version = $Record.GetType().InvokeMember( "StringData", "GetProperty", $Null, $Record, 1 ) 
+        return $Version
+    } catch { 
+        throw "Failed to get MSI Product Version: {0}." -f $_
+    }       
+}
