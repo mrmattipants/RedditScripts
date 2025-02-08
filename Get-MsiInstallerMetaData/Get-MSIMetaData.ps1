c7eaa584fc928fc1c4757db40628383ebca1ee08
Matti Pedersen <mrmattipants@gmail.com>
2025-02-08 10:58:33 -0600

    Add files via upload

---
+++ Get-MsiInstallerMetaData/Get-MSIMetaData.ps1
@@ -0,0 +1,57 @@
+﻿function Get-FileMetaData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline)][Object] $File
+    )
+    Process {
+        foreach ($F in $File) {
+            $MetaDataObject = [ordered] @{}
+            if ($F -is [string]) {
+                $FileInformation = Get-ItemProperty -Path $F
+            } elseif ($F -is [System.IO.DirectoryInfo]) {
+                #Write-Warning "Get-FileMetaData - Directories are not supported. Skipping $F."
+                continue
+            } elseif ($F -is [System.IO.FileInfo]) {
+                $FileInformation = $F
+            } else {
+                Write-Warning "Get-FileMetaData - Only files are supported. Skipping $F."
+                continue
+            }
+            $ShellApplication = New-Object -ComObject Shell.Application
+            $ShellFolder = $ShellApplication.Namespace($FileInformation.Directory.FullName)
+            $ShellFile = $ShellFolder.ParseName($FileInformation.Name)
+            $MetaDataProperties = [ordered] @{}
+            0..400 | ForEach-Object -Process {
+                $DataValue = $ShellFolder.GetDetailsOf($null, $_)
+                $PropertyValue = (Get-Culture).TextInfo.ToTitleCase($DataValue.Trim()).Replace(' ', '')
+                if ($PropertyValue -ne '') {
+                    $MetaDataProperties["$_"] = $PropertyValue
+                }
+            }
+            foreach ($Key in $MetaDataProperties.Keys) {
+                $Property = $MetaDataProperties[$Key]
+                $Value = $ShellFolder.GetDetailsOf($ShellFile, [int] $Key)
+                if ($Property -in 'Attributes', 'Folder', 'Type', 'SpaceFree', 'TotalSize', 'SpaceUsed') {
+                    continue
+                }
+                If (($null -ne $Value) -and ($Value -ne '')) {
+                    $MetaDataObject["$Property"] = $Value
+                }
+            }
+            if ($FileInformation.VersionInfo) {
+                $SplitInfo = ([string] $FileInformation.VersionInfo).Split([char]13)
+                foreach ($Item in $SplitInfo) {
+                    $Property = $Item.Split(":").Trim()
+                    if ($Property[0] -and $Property[1] -ne '') {
+                        $MetaDataObject["$($Property[0])"] = $Property[1]
+                    }
+                }
+            }
+            $MetaDataObject["Attributes"] = $FileInformation.Attributes
+            $MetaDataObject['IsReadOnly'] = $FileInformation.IsReadOnly
+            $MetaDataObject['IsHidden'] = $FileInformation.Attributes -like '*Hidden*'
+            $MetaDataObject['IsSystem'] = $FileInformation.Attributes -like '*System*'
+            [PSCustomObject] $MetaDataObject
+        }
+    }
+}
