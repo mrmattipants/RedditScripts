576b210abe8e1c071e2e4247c2cd03a0c40a6d08
Matti Pedersen <mrmattipants@gmail.com>
2025-03-05 02:04:10 -0600

    Add files via upload

---
+++ Install-MsStorePackages/Install-MsStorePackages_PS5.bat
@@ -0,0 +1,5 @@
+@echo off
+
+powershell.exe -ExecutionPolicy ByPass -File "%~dp0Install-MsStorePackages.ps1"
+
+PAUSE
---
+++ Install-MsStorePackages/Install-MsStorePackages_PS7.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pwsh.exe -ExecutionPolicy ByPass -File "%~dp0Install-MsStorePackages.ps1"
+
+PAUSE
