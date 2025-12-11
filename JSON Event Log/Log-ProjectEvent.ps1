a3f38a2a533a90ac82de462555e8c2c157296d4c
Matti Pedersen <mrmattipants@gmail.com>
2025-12-11 13:16:47 -0600

    Update Log-ProjectEvent.ps1

--- JSON Event Log/Log-ProjectEvent.ps1
+++ JSON Event Log/Log-ProjectEvent.ps1
@@ -54,30 +54,3 @@ Function Log-ProjectEvent {
     }
 
 }
-For testing purposes, I simply use the Current Date/Time for the $StartTime and Add 5 Minutes (300 Seconds) to it, for the $EndTime.
-$ProjectLog = "$([System.Environment]::GetFolderPath('Desktop'))\Project-Event-Log_$(([DateTime]::today).ToString("yyyyddMM")).json"
-$ProjectName = "Project 01"
-$ProjectStart = "$([DateTime]::now)"
-$ProjectEnd = "$(([DateTime]::now).addSeconds(300))"
-$ProjectState = "Running"
-
-Log-ProjectEvent -JsonFilePath $ProjectLog -ProjectName $ProjectName -StartTime $ProjectStart -EndTime $ProjectEnd -CurrentState $ProjectState
-The JSON File Contents will appear as follows.
-{
-  "Entries": [
-    {
-      "ProjectName": "Project 01",
-      "StartTime": "12/11/2025 12:40:55",
-      "EndTime": "12/11/2025 12:45:55",
-      "ElapsedTime": "00:05:00",
-      "CurrentState": "Running"
-    },
-    {
-      "ProjectName": "Project 01",
-      "StartTime": "12/11/2025 12:51:38",
-      "EndTime": "12/11/2025 12:56:38",
-      "ElapsedTime": "00:05:00",
-      "CurrentState": "Running"
-    }
-  ]
-}
