c73c377639c46b86b5cfb7cb2cc42d92a73dd160
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 14:45:27 -0600

    Add files via upload

---
+++ OverrideSecurityRestrictionsOnInsecureOrigin/Google Chrome/GoogleChrome-OverrideSecurityRestrictionsOnInsecureOrigin.ps1
@@ -0,0 +1,36 @@
+﻿$Urls = @(
+'https://[*.]calltrackingmetrics.com/*',
+'http://[*.]calltrackingmetrics.com/*',
+'https://tenant.sharepoint.com/*',
+'https://mytenant.sharepoint.com/*'
+)
+
+$GooglePolicyReg = "REGISTRY::HKEY_LOCAL_MACHINE\Software\Policies\Google\Chrome\OverrideSecurityRestrictionsOnInsecureOrigin"
+
+If (-NOT (Test-Path $GooglePolicyReg -ErrorAction SilentlyContinue)) {
+    New-Item $GooglePolicyReg -Force
+}
+
+$RegItemCount = (Get-Item $GooglePolicyReg).property.count
+if ($RegItemCount -ge 1) {
+    $RegKeyCount = (Get-Item $GooglePolicyReg).property | Select-Object -Last 1
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+
+        New-ItemProperty -Path $GooglePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+
+    }
+
+} Else {
+
+    $RegKeyCount = 0
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+
+        New-ItemProperty -Path $GooglePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+
+    }
+
+}
