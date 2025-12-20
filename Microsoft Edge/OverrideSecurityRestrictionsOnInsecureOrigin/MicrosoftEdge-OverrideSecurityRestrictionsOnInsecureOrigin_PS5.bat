1c813e91a3f073bdf3e8c1af3af573a532a43d58
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 15:24:18 -0600

    Delete Microsoft Edge/OverrideSecurityRestrictionsOnInsecureOrigin directory

--- Microsoft Edge/OverrideSecurityRestrictionsOnInsecureOrigin/MicrosoftEdge-OverrideSecurityRestrictionsOnInsecureOrigin_PS5.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0MicrosoftEdge-OverrideSecurityRestrictionsOnInsecureOrigin.ps1" -WindowStyle Hidden
-
-EXIT
--- Microsoft Edge/OverrideSecurityRestrictionsOnInsecureOrigin/MicrosoftEdge-OverrideSecurityRestrictionsOnInsecureOrigin_PS7.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-C:\Program Files\PowerShell\7\pwsh.exe -ExecutionPolicy ByPass -File "%~dp0MicrosoftEdge-OverrideSecurityRestrictionsOnInsecureOrigin.ps1" -WindowStyle Hidden
-
-EXIT
