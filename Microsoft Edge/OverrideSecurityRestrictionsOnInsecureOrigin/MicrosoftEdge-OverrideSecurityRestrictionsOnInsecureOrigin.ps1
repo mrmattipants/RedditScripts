40b60ee009a9e755dd31f43dc939a8db883c624b
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 15:27:33 -0600

    Add files via upload

---
+++ Microsoft Edge/OverrideSecurityRestrictionsOnInsecureOrigin/MicrosoftEdge-OverrideSecurityRestrictionsOnInsecureOrigin.ps1
@@ -0,0 +1,29 @@
+﻿$Urls = Get-Content -Path "$($PSScriptRoot)\Urls.txt"
+
+$MsEdgePolicyReg = "REGISTRY::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Edge\OverrideSecurityRestrictionsOnInsecureOrigin"
+
+If (-NOT (Test-Path $MsEdgePolicyReg -ErrorAction SilentlyContinue)) {
+    New-Item $MsEdgePolicyReg -Force
+}
+
+$RegItemCount = (Get-Item $MsEdgePolicyReg).property.count
+if ($RegItemCount -ge 1) {
+    $RegKeyCount = (Get-Item $MsEdgePolicyReg).property | Select-Object -Last 1
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+        New-ItemProperty -Path $MsEdgePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+    }
+
+} Else {
+
+    $RegKeyCount = 0
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+
+        New-ItemProperty -Path $MsEdgePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+
+    }
+
+}
