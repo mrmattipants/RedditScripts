607fed5418cf977dc5733751a034765f8cc39136
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 15:04:36 -0600

    Add files via upload

---
+++ Google Chrome/OverrideSecurityRestrictionsOnInsecureOrigin/GoogleChrome-OverrideSecurityRestrictionsOnInsecureOrigin_PS5.bat
@@ -0,0 +1,5 @@
+@echo off
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0GoogleChrome-OverrideSecurityRestrictionsOnInsecureOrigin.ps1" -WindowStyle Hidden
+
+EXIT
