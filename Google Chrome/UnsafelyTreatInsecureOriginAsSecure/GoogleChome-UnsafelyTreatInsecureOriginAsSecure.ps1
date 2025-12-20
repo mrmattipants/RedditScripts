a323a11ba730bc3afcd09cc3fdeb4fac4f592225
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 15:29:01 -0600

    Add files via upload

---
+++ Google Chrome/UnsafelyTreatInsecureOriginAsSecure/GoogleChome-UnsafelyTreatInsecureOriginAsSecure.ps1
@@ -0,0 +1,32 @@
+﻿
+$Urls = Get-Content -Path "$($PSScriptRoot)\Urls.txt"
+
+$GooglePolicyReg = "REGISTRY::HKEY_LOCAL_MACHINE\Software\Policies\Google\Chrome\UnsafelyTreatInsecureOriginAsSecure"
+
+If (-NOT (Test-Path $GooglePolicyReg -ErrorAction SilentlyContinue)) {
+    New-Item $GooglePolicyReg -Force
+}
+
+$RegItemCount = (Get-Item $GooglePolicyReg).property.count
+if ($RegItemCount -ge 1) {
+    $RegKeyCount = (Get-Item $GooglePolicyReg).property | Select-Object -Last 1
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+
+        New-ItemProperty -Path $GooglePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+
+    }
+
+} Else {
+
+    $RegKeyCount = 0
+
+    Foreach ($Url in $Urls) {
+        [string]$RegKeyCount = [int]$RegKeyCount + 1
+
+        New-ItemProperty -Path $GooglePolicyReg -Name $RegKeyCount -PropertyType String -Value $Url -Force
+
+    }
+
+}
