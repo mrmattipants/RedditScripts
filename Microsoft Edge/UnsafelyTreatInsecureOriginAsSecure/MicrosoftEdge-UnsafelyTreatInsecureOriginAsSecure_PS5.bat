21761ccf5579579645fe414742816da26ccd24ea
Matti Pedersen <mrmattipants@gmail.com>
2025-12-20 15:28:01 -0600

    Add files via upload

---
+++ Microsoft Edge/UnsafelyTreatInsecureOriginAsSecure/MicrosoftEdge-UnsafelyTreatInsecureOriginAsSecure_PS5.bat
@@ -0,0 +1,5 @@
+@echo off
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0MicrosoftEdge-UnsafelyTreatInsecureOriginAsSecure.ps1" -WindowStyle Hidden
+
+EXIT
